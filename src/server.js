cbfc691f964c0a20f41c5fed56eab3f14e325020
Pariskrit12 <bhagataryan784@gmail.com>
2025-11-21 21:38:52 +0545

    Added DB Connection

--- src/server.js
+++ src/server.js
@@ -1,10 +1,17 @@
 import http from "http";
 import { app } from "./app.js";
 import dotenv from "dotenv";
+import connectDB from "./DB/connectDB.js";
 
 dotenv.config({ path: "./env" });
 const server = http.createServer(app);
 
-server.listen(process.env.PORT, () => {
-  console.log(`Server running on PORT:${process.env.PORT}`);
-});
+connectDB()
+  .then(() => {
+    server.listen(process.env.PORT, () => {
+      console.log(`Server running on PORT:${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB Failed To connect", err);
+  });
